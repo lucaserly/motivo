9e3497b96db0e4d3c8ab1a9a6dd2395aeaf0a4d4
Lucas <l.erlacher@icloud.com>
2021-11-26 16:34:31 +0100

    added express static dirname

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const router = require('./router');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5002;
 const db = require('./models');
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(cors());
 app.use(express.json());
 app.use(router);
