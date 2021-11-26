23c1f546b8286c1332e2aa11aa44f5d27b03ab91
Lucas <l.erlacher@icloud.com>
2021-11-26 17:09:44 +0100

    removed localhost from url

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import { ExpensesList } from './components/';
 import { useFetch } from './custom_hooks/useFetch';
-const URL = 'http://localhost:5002/expenses';
-const URL_2 = 'http://localhost:5002/greeting';
+// const URL = 'http://localhost:5002/expenses';
+// const URL_2 = 'http://localhost:5002/greeting';
+const URL = '/expenses';
+const URL_2 = '/greeting';
 
 function App() {
   const { response } = useFetch(URL, {});
