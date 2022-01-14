3372a5b54e99e8e1df26da983c25f629845fafc9
Lucas <l.erlacher@icloud.com>
2022-01-14 16:07:36 +0100

    converted year to two digits

--- client/src/containers/BulkDownload/BulkDownload.js
+++ client/src/containers/BulkDownload/BulkDownload.js
@@ -17,22 +17,28 @@ const parseDate = (date) => {
   return `${splittedDate[1]}/ ${splittedDate[0]}/${splittedDate[2]}`;
 };
 
+const removeTwoDigitsFromYear = (date) => {
+  const splittedDate = date.split('/');
+  const splittedYear = splittedDate[2];
+  return `${splittedDate[0]}/${splittedDate[1]}/${splittedYear[2]}${splittedYear[3]}`;
+};
+
 export const BulkDownload = ({ expenses }) => {
   const handleDownload = () => {
-    const parsedData = expenses.map((expense) => ({
+    const sortedData = expenses.sort(
+      (a, b) => new Date(parseDate(a.date)) - new Date(parseDate(b.date))
+    );
+
+    const parsedData = sortedData.map((expense) => ({
       Item: expense.item,
       Category: expense.category,
       Description: expense.description,
       Payment: expense.payment,
       Amount: Number(expense.amount.split(' ')[0]),
-      Date: expense.date,
+      Date: removeTwoDigitsFromYear(expense.date),
     }));
 
-    const sortedData = parsedData.sort(
-      (a, b) => new Date(parseDate(a.Date)) - new Date(parseDate(b.Date))
-    );
-
-    const sheet = XLSX.utils.json_to_sheet(sortedData, { headers });
+    const sheet = XLSX.utils.json_to_sheet(parsedData, { headers });
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, sheet, 'Expenses');
 
